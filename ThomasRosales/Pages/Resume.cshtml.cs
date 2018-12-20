918fb9bc39b0f1bafa542fb0d9a090aff3fe1e3e
Thomas Rosales <trosales@referralrock.com>
2018-12-20 15:10:44 +0800

    Updates add Resume page

---
+++ ThomasRosales/Pages/Resume.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThomasRosales.Pages
+{
+    public class Resume : PageModel
+    {
+        public string Message { get; set; }
+        public string CurrentPage { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+            CurrentPage = "Resume";
+        }
+    }
+}
